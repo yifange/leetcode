10f3272f86c50db5c5b21f356b0808a07add1a41
yifange <irfan.1989@gmail.com>
2014-02-23 17:35:43 -0500

    two sum

--- two_sum.py
+++ two_sum.py
@@ -3,27 +3,20 @@ class Solution:
         nums = []
         for i in range(len(numbers)):
             nums.append((numbers[i], i))
-        for i in range(len(numbers) - 1):
-            for j in range(i + 1, len(numbers)):
-                if nums[i][0] > nums[j][0]:
-                    t = nums[i]
-                    nums[i] = nums[j]
-                    nums[j] = t
-
-        for i in range(len(numbers) - 1):
-            if nums[i][0] > target:
-                break
-            for j in range(i + 1, len(numbers)):
-                if nums[i][0] + nums[j][0] == target:
-                    if nums[i][1] < nums[j][1]:
-                        return [nums[i][1] + 1, nums[j][1] + 1]
-                    else:
-                        return [nums[j][1] + 1, nums[i][1] + 1]
-
-                if nums[i][0] + nums[j][0] > target:
-                    break
-
+        nums.sort()
+        i = 0
+        j = len(nums) - 1
+        while i < j:
+            if nums[i][0] + nums[j][0] < target:
+                i += 1
+            elif nums[i][0] + nums[j][0] > target:
+                j -= 1
+            else:
+                r = [nums[i][1] + 1, nums[j][1] + 1]
+                r.sort()
+                return r
+        return [-1, -1]
 sol = Solution()
-numbers = [0, 4, 3, 0]
-target = 0
+numbers = [3, 2, 4]
+target = 6
 print sol.twoSum(numbers, target)
